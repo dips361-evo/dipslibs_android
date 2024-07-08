66c3325eb280a761bda4b132d0c392f1834e38dd
mohammadrafii480 <mohammadrafii480@gmail.com>
2024-07-08 14:36:38 +0700

    Upgrade version fixing route'

--- app/src/main/java/com/evo/mitzoom/ui/DipsChooseLanguage.java
+++ app/src/main/java/com/evo/mitzoom/ui/DipsChooseLanguage.java
@@ -136,7 +136,7 @@ public class DipsChooseLanguage extends AppCompatActivity {
 
         try {
             PackageInfo info = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(),0);
-            String version = "1.0.35";
+            String version = "1.0.36";
             version = "V "+version;
             tvVersion.setText(version);
         } catch (PackageManager.NameNotFoundException e) {
